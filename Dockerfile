e251c026d006c3a422570e5948eb2d23e0c4215c
Timeless Residents <business@satotakuya.jp>
2025-02-16 12:56:27 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# Dockerfile
 FROM gitlab/gitlab-ce:latest
 
 # システムの依存関係をインストール
@@ -8,17 +9,8 @@ RUN apt-get update && apt-get install -y \
 # タイムゾーンを設定
 ENV TZ=Asia/Tokyo
 
-# GitLab設定
-ENV GITLAB_OMNIBUS_CONFIG="external_url 'https://handson-gitlab.onrender.com'; \
-    puma['worker_processes']=2; \
-    puma['min_threads']=1; \
-    puma['max_threads']=4; \
-    postgresql['shared_buffers']='128MB'; \
-    postgresql['max_connections']=100; \
-    nginx['worker_processes']=2; \
-    sidekiq['concurrency']=5; \
-    prometheus_monitoring['enable']=false; \
-    nginx['listen_port']=ENV['PORT']"
+# GitLab設定ファイルをコピー
+COPY gitlab.rb /etc/gitlab/gitlab.rb
 
 # ポートの設定
 EXPOSE ${PORT:-80}
--- gitlab.rb
+++ gitlab.rb
@@ -1,28 +1,30 @@
 # 基本URL設定
 external_url 'https://handson-gitlab.onrender.com'
 
-# Pumaの最小設定
+# Pumaの設定
 puma['worker_processes'] = 2
 puma['min_threads'] = 1
 puma['max_threads'] = 4
 
-# PostgreSQLの最小設定
+# PostgreSQLの設定
 postgresql['enable'] = true
 postgresql['shared_buffers'] = "128MB"
+postgresql['max_connections'] = 100
 
-# Redisの最小設定
+# Redisの設定
 redis['maxmemory'] = "256mb"
 redis['maxmemory_policy'] = "allkeys-lru"
 
-# Nginxの最小設定
+# Nginxの設定
 nginx['worker_processes'] = 2
+nginx['listen_port'] = ENV['PORT'] || 80
 
-# Sidekiqの最小設定
+# Sidekiqの設定
 sidekiq['concurrency'] = 5
 
 # メモリ使用量の最適化
-postgresql['max_connections'] = 100
 prometheus_monitoring['enable'] = false
 
-# Render.com用のポート設定
-nginx['listen_port'] = ENV['PORT'] || 80
+# GitLab Workhorse設定
+gitlab_workhorse['listen_network'] = "tcp"
+gitlab_workhorse['listen_addr'] = "0.0.0.0:#{ENV['PORT'] || 80}"
